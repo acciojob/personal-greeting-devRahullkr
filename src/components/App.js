dc1638d5eb640aaa34a6dd1f634d693dee983940
Rahulllkr <rahulkumar8604745@gmail.com>
2026-01-04 23:36:59 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
         {/* Do not remove the main div */}
         <h1>Enter your name:</h1>
         <input onChange={(e) => setName(e.target.value)} value={name} type="text" />
-        { name ? <h1>Hello,{name}!</h1> : ""}
+        { name ? <h1>"Hello,{name}!"</h1> : ""}
     </div>
   )
 }
