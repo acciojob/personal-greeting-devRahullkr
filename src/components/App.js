39b1b1005ad7de7203cb9ffef1fbcd8d18a3ddb5
Rahulllkr <rahulkumar8604745@gmail.com>
2026-01-04 23:30:21 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const []
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1></h1>
     </div>
   )
 }
